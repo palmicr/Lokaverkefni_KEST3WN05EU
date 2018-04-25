ca25388005832b21863c0eb87fac5fb88821bd5f
Pálmi C Rúnarsson <pancakeprins@gmail.com>
2018-04-25 17:28:54 +0000

    new wile

--- Lokaverkefni_Uppsetning.ps1
+++ Lokaverkefni_Uppsetning.ps1
@@ -1,4 +1,4 @@
-﻿functon 1-time-groups {
+﻿function 1-time-groups {
 ## Basic Groups
 New-ADOrganizationalUnit -Name Notendur -ProtectedFromAccidentalDeletion $false
 $notOUpath = (Get-ADOrganizationalUnit -Filter { name -like 'Notendur' }).DistinguishedName
@@ -14,9 +14,12 @@ New-ADGroup -Name AllirNemendur -Path $nemOUpath -GroupScope Global
 ##############################################
 }
 
-functon CreateUsers {
-$notendur = Import-Csv .\lokaverk_notendur.csv
+function CreateUsers {
+$notendur = Import-Csv .\downloads\lokaverk_notendur.csv
 ##############################################
+$notOUpath = (Get-ADOrganizationalUnit -Filter { name -like 'Notendur' }).DistinguishedName
+$starfOUpath = (Get-ADOrganizationalUnit -Filter { name -like 'Starfsmenn' }).DistinguishedName
+$nemOUpath = (Get-ADOrganizationalUnit -Filter { name -like 'Nemendur' }).DistinguishedName
 #Groups
     foreach ($s in $notendur){            
         $skoli = $s.Skoli      
@@ -26,18 +29,21 @@ $notendur = Import-Csv .\lokaverk_notendur.csv
         ##############################################
         ## Búa til Nemendu OU.
         if($hlutverk -eq "Nemendur"){
-            $skolOUpath = (Get-ADOrganizationalUnit -SearchBase $nemOUpath -Filter { name -like $skoli }).DistinguishedName
+            $skolOUpath = (Get-ADOrganizationalUnit -SearchBase $nemOUpath -Filter { name -like $skoli }).DistinguishedName 
             $brautOUpath = (Get-ADOrganizationalUnit -SearchBase $nemOUpath -Filter { name -like $braut }).DistinguishedName
+           
 
-            if(-not(Get-ADOrganizationalUnit -Path $nemOUpath -Filter { name -like $skoli })) {
-            New-ADOrganizationalUnit -Name $skoli -Path $nemOUpath -ProtectedFromAccidentalDeletion $false
-            New-ADGroup -Name $skoli -Path $("ou=" + $skoli + "," + $skolOUpath) -GroupScope Global
-            Add-ADGroupMember -Identity AllirNemendur -Members $skoli 
+            if(-not(Get-ADOrganizationalUnit -SearchBase $nemOUpath -Filter { name -like $skoli })) {
+                New-ADOrganizationalUnit -Name $skoli -Path $nemOUpath -ProtectedFromAccidentalDeletion $false
+                $skolOUpath = (Get-ADOrganizationalUnit -SearchBase $nemOUpath -Filter { name -like $skoli }).DistinguishedName 
+                New-ADGroup -Name $($skoli + "_" + $hlutverk) -Path $skolOUpath -GroupScope Global
+                Add-ADGroupMember -Identity AllirNemendur -Members $($skoli + "_" + $hlutverk) 
             }
-            if(-not(Get-ADOrganizationalUnit -Path $skolOUpath -Filter { name -like $braut })) {
-            New-ADOrganizationalUnit -Name $braut -Path $skolOUpath -ProtectedFromAccidentalDeletion $false
-            New-ADGroup -Name $braut -Path $("ou=" + $skoli + "," + $brautOUpath) -GroupScope Global
-            Add-ADGroupMember -Identity $skoli -Members $braut  
+            if(-not(Get-ADOrganizationalUnit -SearchBase $skolOUpath -Filter { name -like $braut })) {
+                New-ADOrganizationalUnit -Name $braut -Path $skolOUpath -ProtectedFromAccidentalDeletion $false
+                $brautOUpath = (Get-ADOrganizationalUnit -SearchBase $nemOUpath -Filter { name -like $braut }).DistinguishedName
+                New-ADGroup -Name $($braut + "_" + $hlutverk) -Path $brautOUpath -GroupScope Global
+                Add-ADGroupMember -Identity $($skoli + "_" + $hlutverk) -Members $($braut + "_" + $hlutverk)  
             }
         }
         ##############################################
@@ -47,48 +53,102 @@ $notendur = Import-Csv .\lokaverk_notendur.csv
             $hlutOUpath = (Get-ADOrganizationalUnit -SearchBase $starfOUpath -Filter { name -like $hlutverk }).DistinguishedName
             $brautOUpath = (Get-ADOrganizationalUnit -SearchBase $starfOUpath -Filter { name -like $braut }).DistinguishedName
 
-            if(-not(Get-ADOrganizationalUnit -Path $starfOUpath -Filter { name -like $hlutverk })) {
-            New-ADOrganizationalUnit -Name $hlutverk -Path $starfOUpath -ProtectedFromAccidentalDeletion $false
-            New-ADGroup -Name $hlutverk -Path $("ou=" + $hlutverk + "," + $hlutOUpath) -GroupScope Global
-            Add-ADGroupMember -Identity AllirStarsmenn -Members $hlutverk 
+            if(-not(Get-ADOrganizationalUnit -SearchBase $starfOUpath -Filter { name -like $hlutverk })) {
+                New-ADOrganizationalUnit -Name $hlutverk -Path $starfOUpath -ProtectedFromAccidentalDeletion $false
+                $hlutOUpath = (Get-ADOrganizationalUnit -SearchBase $starfOUpath -Filter { name -like $hlutverk }).DistinguishedName
+                New-ADGroup -Name $hlutverk -Path $hlutOUpath -GroupScope Global
+                Add-ADGroupMember -Identity AllirStarfsmenn -Members $hlutverk 
             }
-            if(-not(Get-ADOrganizationalUnit -Path $hlutOUpath -Filter { name -like $skoli })) {
-            New-ADOrganizationalUnit -Name $skoli -Path $hlutOUpath -ProtectedFromAccidentalDeletion $false
-            New-ADGroup -Name $skoli -Path $("ou=" + $skoli + "," + $skolOUpath) -GroupScope Global
-            Add-ADGroupMember -Identity $hlutverk -Members $skoli 
+            if(-not(Get-ADOrganizationalUnit -SearchBase $hlutOUpath -Filter { name -like $skoli })) {
+                New-ADOrganizationalUnit -Name $skoli -Path $hlutOUpath -ProtectedFromAccidentalDeletion $false
+                $skolOUpath = (Get-ADOrganizationalUnit -SearchBase $starfOUpath -Filter { name -like $skoli }).DistinguishedName
+                New-ADGroup -Name $($skoli + "_" + $hlutverk) -Path $skolOUpath -GroupScope Global
+                Add-ADGroupMember -Identity $hlutverk -Members $($skoli + "_" + $hlutverk) 
             }
-            if(-not(Get-ADOrganizationalUnit -Path $skolOUpath -Filter { name -like $braut })) {
-            New-ADOrganizationalUnit -Name $braut -Path $skolOUpath -ProtectedFromAccidentalDeletion $false
-            New-ADGroup -Name $braut -Path $("ou=" + $skoli + "," + $brautOUpath) -GroupScope Global
-            Add-ADGroupMember -Identity $skoli -Members $braut
+            if(-not(Get-ADOrganizationalUnit -SearchBase $skolOUpath -Filter { name -like $braut })) {
+                New-ADOrganizationalUnit -Name $braut -Path $skolOUpath -ProtectedFromAccidentalDeletion $false
+                $brautOUpath = (Get-ADOrganizationalUnit -SearchBase $starfOUpath -Filter { name -like $braut }).DistinguishedName
+                New-ADGroup -Name $($braut + "_" + $hlutverk) -Path $brautOUpath -GroupScope Global
+                Add-ADGroupMember -Identity $($skoli + "_" + $hlutverk) -Members $($braut + "_" + $hlutverk)
             }
         }
         ##############################################
         #user
-                 $hashUser = @{
-                 Name = $n.nafn 
-                 Title = $n.starfsheiti
-                 State = $n.sveitarfelag
-                 DisplayName = $n.nafn 
-                 GivenName = $n.fornafn 
-                 Surname = $n.eftirnafn
-                 HomePhone = $n.heimasimi
-                 OfficePhone =  $n.vinnusimi
-                 MobilePhone = $n.farsimi
-                 Department = $n.deild 
-                 SamAccountName = $n.notendanafn 
-                 UserPrincipalName = $($n.notendanafn + "@" + $env:USERDNSDOMAIN) 
-                 Path = $("ou=" + $deild + "," + $grunnOUpath) 
-                 AccountPassword = (ConvertTo-SecureString -AsPlainText "pass.123" -Force) 
-                 Enabled = $true
+                if($hlutverk -eq "Nemendur"){
+                    $stada = "Nemandi"
+                    $n = $s.Nafn.ToLower()
+                    $n = $n -replace 'á','a'
+                    $n = $n -replace 'ú','u'
+                    $n = $n -replace 'ð','d'
+                    $n = $n -replace 'í','i'
+                    $n = $n -replace 'ó','o'
+                    $n = $n -replace 'ú','u'
+                    $n = $n -replace 'þ','th'
+                    $n = $n -replace 'æ','ae'
+                    $n = $n -replace 'ö','o'
+                    $n = $n -replace 'ý','y'
+                    $a = $($n.Substring(0,2) + $n.Split(" ")[-1].Substring(0,2))
+                    $teljari = 1
+                    if(-not(Get-ADUser -Filter "SamAccountName -like '$a*'")){
+                            $a = $($a + $teljari)
+                    }
+                    else{
+                        $finnanotandi = (Get-ADUser -Filter "SamAccountName -like '$a*'")
+                        foreach($fannst in $finnanotandi.SamAccountName){
+                            $teljari++
+                            $teljari
+                        }
+                        $a = $($a + $teljari)
+                        $a
+                        } 
+                    $username = $a;
+                    $username
                 }
-
-        New-ADUser @hashUser
+                else{
+                    $stada = "Kennari"
+                    $n = $s.Nafn.ToLower()
+                    $n = $n -replace 'á','a'
+                    $n = $n -replace 'ú','u'
+                    $n = $n -replace 'ð','d'
+                    $n = $n -replace 'í','i'
+                    $n = $n -replace 'ó','o'
+                    $n = $n -replace 'ú','u'
+                    $n = $n -replace 'þ','th'
+                    $n = $n -replace 'æ','ae'
+                    $n = $n -replace 'ö','o'
+                    $n = $n -replace 'ý','y'
+                    $n = $n -replace ' ','.'
+                    if($n.length -gt 20){
+                        $n = $n.Substring(0,20)
+                    }
+                    $username = $n;
+                }
+                $name = $s.Nafn.Split(" ")
+                if($name.length -eq 2){
+                    $givenname = $name[0]
+                }
+                else{
+                    $givenname = $name[0, -2]
+                }
+                $hashUser = @{
+                    'Name'= $s.Nafn;
+                    'Displayname'= $s.Nafn;
+                    'Title' = $stada;
+                    'Givenname' = "$givenname";
+                    'Surname'= $s.Nafn.Split(" ")[-1];
+                    'Samaccountname'= $username;
+                    'UserPrincipalName' = $($username + "@" + $env:USERDNSDOMAIN);
+                    'Path'= $brautOUpath;
+                    'AccountPassword'= (ConvertTo-SecureString -AsPlainText "pass.123" -Force);
+                    'Enabled' = $true;
+                 }
+                 New-ADUser @hashUser
+                 Add-ADGroupMember -Identity $($braut + "_" + $hlutverk) -Members $username
         ##############################################
         #Create Web and Sql
-        if($hlutverk -eq "Nemandi"){
+        #if($hlutverk -eq "Nemandi"){
 
-        }
+        #}
         ##############################################
         #Create Sql
 
